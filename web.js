ca64d66a723f492fe964c3add8d48b18861252ff
Asif Rafique <asif.rafique@gmail.com>
2013-07-25 11:57:55 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
-
+var readFile = function(filename) {
+var fileData = null;
+    fs.readFileSync('index.html',function(err,data) {
+	if(err) console.log(err);
+	fileData = data;
+});
+return fileData;
+}
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+    resonnse.send(readFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
